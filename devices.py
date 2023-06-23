cf2c68d9917badc156922a93bd72ece19ab21284
Aleš Kučera <kuceral4@fel.cvut.cz>
2023-06-23 17:51:17 +0200

    BFS

--- devices.py
+++ devices.py
@@ -1,62 +1,202 @@
-class LineDegreeROADM(object):
+from collections import deque
+
+
+class Device(object):
     def __init__(self, name: str):
         self.name = name
-        self.line_conn = None
-        self.express_conn = {i: None for i in range(10)}
 
-    @property
-    def children(self):
-        return list(self.express_conn.values())
+    def neighbors(self, in_connection: str = None):
+        raise NotImplementedError
 
-    def __eq__(self, other):
-        return self.name == other.name and type(self) == type(other)
+    def get_path(self, target):
+        visited, frontier = set(), deque()
 
-
-class AddDropROADM(object):
-    def __init__(self, name: str, parent=None):
-        self.name = name
-        self.parent = parent
-        self.express_conn = {i: None for i in range(8)}
-        self.client_conn = {i: None for i in range(20)}
-
-    @property
-    def children(self):
-        return list(self.express_conn.values()) + list(self.client_conn.values())
-
-    def __eq__(self, other):
-        return self.name == other.name and type(self) == type(other)
-
-
-class Client(object):
-    def __init__(self, name: str):
-        self.name = name
-        self.client_conn = None
-
-    def __eq__(self, other):
-        return self.name == other.name and type(self) == type(other)
-
-    def find_path(self, target):
-        frontier = {self.client_conn: self}
-        visited = []
+        initial_state = State(device=self, in_connection=None, parent=None)
+        frontier.extend(initial_state.children())
 
         while frontier:
-            current = frontier.pop()
-            if current == target:
-                return True
-            visited.append(current)
-            for child in current.children:
-                if child not in visited:
-                    frontier.append(child)
+            current_state = frontier.popleft()
+            if current_state.is_target(target):
+                return current_state.construct_path()
+
+            if current_state not in visited:
+                visited.add(current_state)
+                frontier.extend(current_state.children())
+
+        return False
+
+    def __eq__(self, other):
+        return self.name == other.name and type(self) == type(other)
+
+    def __repr__(self):
+        return f'{self.name} - {type(self).__name__}'
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __hash__(self):
+        return hash((self.name, type(self)))
+
+
+class State(object):
+    def __init__(self, device: Device, in_connection: str = None, parent=None):
+        self.device = device
+        self.parent = parent
+        self.in_connection = in_connection
+
+    def children(self):
+        devices, connection_types = self.device.neighbors(self.in_connection)
+        return [State(d, c, self) for d, c in zip(devices, connection_types)]
+
+    def is_target(self, target: Device):
+        return self.device == target
+
+    def construct_path(self):
+        path, state = [], self
+        while state.parent is not None:
+            path.append(state)
+            state = state.parent
+        path.append(state)
+        return path[::-1]
+
+    def __eq__(self, other):
+        return self.device == other.device and self.in_connection == other.in_connection
+
+    def __repr__(self):
+        return f'{self.in_connection} - {self.device.name}'
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __hash__(self):
+        return hash((self.device, self.in_connection))
+
+
+class LineDegreeROADM(Device):
+    def __init__(self, name: str):
+        super().__init__(name)
+        self.line_conn = None
+        self.intra_conn = {f'port_{i + 1}': None for i in range(10)}
+
+    @property
+    def num_intra_conn(self):
+        return len([d for d in self.intra_conn.values() if d is not None])
+
+    def neighbors(self, in_connection: str = None):
+        devices, connection_types = [], []
+
+        if in_connection == 'LineConnection' or in_connection is None:
+            devices.extend([d for d in self.intra_conn.values() if d is not None])
+            connection_types.extend(['IntraConnection'] * self.num_intra_conn)
+
+        if in_connection == 'IntraConnection' or in_connection is None:
+            devices.append(self.line_conn)
+            connection_types.append('LineConnection')
+
+        return devices, connection_types
+
+
+class AddDropROADM(Device):
+    def __init__(self, name: str):
+        super().__init__(name)
+        self.intra_conn = {f'port_{i + 1}': None for i in range(8)}
+        self.client_conn = {f'port_{i + 1}': None for i in range(20)}
+
+    @property
+    def num_intra_conn(self):
+        return len([d for d in self.intra_conn.values() if d is not None])
+
+    @property
+    def num_client_conn(self):
+        return len([d for d in self.client_conn.values() if d is not None])
+
+    def neighbors(self, in_connection: str = None):
+        devices, connection_types = [], []
+
+        if in_connection == 'IntraConnection' or in_connection is None:
+            devices.extend([d for d in self.client_conn.values() if d is not None])
+            connection_types.extend(['ClientConnection'] * self.num_client_conn)
+
+        if in_connection == 'ClientConnection' or in_connection is None:
+            devices.extend([d for d in self.intra_conn.values() if d is not None])
+            connection_types.extend(['IntraConnection'] * self.num_intra_conn)
+
+        return devices, connection_types
+
+
+class Client(Device):
+    def __init__(self, name: str):
+        super().__init__(name)
+        self.client_conn = None
+
+    def neighbors(self, in_connection: str = None):
+        devices, connection_types = [], []
+
+        if in_connection is None:
+            devices.append(self.client_conn)
+            connection_types.append('ClientConnection')
+
+        return devices, connection_types
 
 
 if __name__ == '__main__':
-    client1 = Client('client1')
-    client2 = Client('client2')
+    ld1 = LineDegreeROADM('ld1')
+    ld2 = LineDegreeROADM('ld2')
+    ld3 = LineDegreeROADM('ld3')
+    ld4 = LineDegreeROADM('ld4')
+    ld5 = LineDegreeROADM('ld5')
+    ld6 = LineDegreeROADM('ld6')
 
     ad1 = AddDropROADM('ad1')
     ad2 = AddDropROADM('ad2')
+    ad3 = AddDropROADM('ad3')
 
-    ld1 = LineDegreeROADM('ld1')
-    ld2 = LineDegreeROADM('ld2')
+    client1 = Client('client1')
+    client2 = Client('client2')
+    client3 = Client('client3')
+    client4 = Client('client4')
 
-    print(client1 == ad1)
+    # Configure connections
+    ld1.line_conn = ld5
+    ld1.intra_conn[0] = ld2
+    ld1.intra_conn[1] = ad1
+
+    ld2.line_conn = ld3
+    ld2.intra_conn[0] = ld1
+    ld2.intra_conn[1] = ad1
+
+    ld3.line_conn = ld2
+    ld3.intra_conn[0] = ld4
+    ld3.intra_conn[1] = ad2
+
+    ld4.line_conn = ld6
+    ld4.intra_conn[0] = ld3
+    ld4.intra_conn[1] = ad2
+
+    ld5.line_conn = ld1
+    ld5.intra_conn[0] = ld6
+    ld5.intra_conn[1] = ad3
+
+    ld6.line_conn = ld4
+    ld6.intra_conn[0] = ld5
+    ld6.intra_conn[1] = ad3
+
+    ad1.intra_conn[0] = ld1
+    ad1.intra_conn[1] = ld2
+    ad1.client_conn[0] = client1
+
+    ad2.intra_conn[0] = ld3
+    ad2.intra_conn[1] = ld4
+    ad2.client_conn[0] = client2
+
+    ad3.intra_conn[0] = ld5
+    ad3.intra_conn[1] = ld6
+    ad3.client_conn[0] = client3
+    ad3.client_conn[1] = client4
+
+    client1.client_conn = ad1
+    client2.client_conn = ad2
+    client3.client_conn = ad3
+    client4.client_conn = ad3
+
+    print(client1.get_path(client4))
--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from pyvis.network import Network
 
 from devices import LineDegreeROADM, AddDropROADM, Client
 
-device_colors = {'LineDegreeROADM': '#22577a',
-                 'AddDropROADM': '#57cc99',
-                 'Client': '#c7f9cc'}
+device_colors = {'LineDegreeROADM': '#124559',
+                 'AddDropROADM': '#598392',
+                 'Client': '#d90368'}
 
-connection_colors = {'LongConnection': '#4f000b',
-                     'ExpressConnection': '#720026',
-                     'ClientConnection': '#ce4257'}
+connection_colors = {'line_connections': '#c1121f',
+                     'intra_connections': '#72b01d',
+                     'client_connections': '#fb5607'}
 
 
 def main():
@@ -17,28 +17,54 @@ def main():
         data = yaml.safe_load(f)
 
     net = Network()
+    devices = []
+    device_names = []
 
     for i, device in enumerate(data):
-        assert device['type'] in device_colors.keys()
-        net.add_node(i, label=device['name'], title=device['type'], color=device_colors[device['type']])
+        dev_type = device['type']
+        if dev_type == 'LineDegreeROADM':
+            device_names.append(device['name'])
+            devices.append(LineDegreeROADM(device['name']))
+            net.add_node(i, label=device['name'], title=dev_type, color=device_colors[dev_type], shape='square')
+        elif dev_type == 'AddDropROADM':
+            device_names.append(device['name'])
+            devices.append(AddDropROADM(device['name']))
+            net.add_node(i, label=device['name'], title=dev_type, color=device_colors[dev_type], shape='square')
+        elif dev_type == 'Client':
+            device_names.append(device['name'])
+            devices.append(Client(device['name']))
+            net.add_node(i, label=device['name'], title=dev_type, color=device_colors[dev_type], shape='dot')
 
-    net.show('graph.html')  # save visualization in 'graph.html'
+    for i, (dev_data, device) in enumerate(zip(data, devices)):
+        dev_type = dev_data['type']
+        if dev_type == 'LineDegreeROADM':
+            for port, dev_name in dev_data['intra_connections'].items():
+                device.intra_conn[port] = devices[device_names.index(dev_name)]
+                net.add_edge(i, device_names.index(dev_name), color=connection_colors['intra_connections'], width=4)
+
+            device.line_conn = devices[device_names.index(dev_data['line_connection'])]
+            net.add_edge(i, device_names.index(dev_data['line_connection']),
+                         color=connection_colors['line_connections'], width=4)
+
+        elif dev_type == 'AddDropROADM':
+            for port, dev_name in dev_data['intra_connections'].items():
+                device.intra_conn[port] = devices[device_names.index(dev_name)]
+                net.add_edge(i, device_names.index(dev_name), color=connection_colors['intra_connections'], width=4)
+            for port, dev_name in dev_data['client_connections'].items():
+                device.client_conn[port] = devices[device_names.index(dev_name)]
+                net.add_edge(i, device_names.index(dev_name), color=connection_colors['client_connections'], width=4)
+
+        elif dev_type == 'Client':
+            device.client_conn = devices[device_names.index(dev_data['client_connection'])]
+            net.add_edge(i, device_names.index(dev_data['client_connection']),
+                         color=connection_colors['client_connections'], width=4)
+
+    net.show('graph.html')
+
+    print(devices)
+    path = devices[-1].get_path(devices[-3])
+    print(path)
 
 
 if __name__ == '__main__':
     main()
-
-# net = Network()  # create graph object
-#
-# # add nodes
-# net.add_nodes(
-#     [1, 2, 3, 4, 5],  # node ids
-#     label=['Node #1', 'Node #2', 'Node #3', 'Node #4', 'Node #5'],  # node labels
-#     # node titles (display on mouse hover)
-#     title=['Main node', 'Just node', 'Just node', 'Just node', 'Node with self-loop'],
-#     color=['#d47415', '#22b512', '#42adf5', '#4a21b0', '#e627a3']  # node colors (HEX)
-# )
-# # add list of edges, same as in the previous example
-# net.add_edges([(1, 2), (1, 3), (2, 3), (2, 4), (3, 5), (5, 5)])
-#
-# net.show('graph.html')  # save visualization in 'graph.html'
