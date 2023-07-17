bb1ad50d6e29c57ab5d4d3e13a1c866049a2ad9a
Aleš Kučera <kuceral4@fel.cvut.cz>
2023-07-17 07:56:23 +0200

    Notes

--- apps/device_config.py
+++ apps/device_config.py
@@ -6,7 +6,22 @@ device_types = [
     ('alien_wavelength_add_drop', 'Alien Wavelength Add/Drop ROADM'),
     ('client', 'Client'),
 ]
+# Add line, add, then add types
+# client separately, automatically
+# intra client manually
+# separate devices and termination points
+# find path, manage termination points
 
+# termination point 1, termination point 2
+# TD filtration destination
+
+# Nodes. ROADM?
+
+# subtype
+
+# optical connection, (TP1, TP2), path, paths found, simulation result
+
+# Node management
 device_ports = {
     'line_degree': {
         'line': 1,
